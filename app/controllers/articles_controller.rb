fea0bca4be17321676704a8086ef5dc92ef9353c
Ahmet Kaptan <38915226+codescaptain@users.noreply.github.com>
2022-04-10 00:36:09 +0300

    Article,  Handle validation errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,30 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    binding.break
+    article = Article.new(article_params)
+    if article.valid?
+
+    else
+      render json: { "errors": errors(article) }, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+
+  def article_params
+    ActionController::Parameters.new
+  end
+
+  def errors(article)
+    errors = []
+    article.errors.each do |attr, msg|
+      errors << {
+        source: { pointer: "/data/attributes/#{attr.to_s}" },
+        detail: msg
+      }
+    end
+    errors
   end
 
 end
